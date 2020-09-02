8ca46392f4f07274f6e8692a66d67b6c4eb3b2fd
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2020-09-02 21:13:22 +0300

    force numeric props

--- Vasya_-_Clerk.js
+++ Vasya_-_Clerk.js
@@ -2,21 +2,17 @@ const PRICE = 25;
 
 // Transcation status codes
 const TR = 'OK NO_CHANGE INSUFFICIENT FAIL'.split(/\s+/).reduce(
-    (acc, e, i) => {
-        acc[e] = i;
-        return acc;
-    }, {}
+    (acc, e, i) => (acc[e] = i, acc), {}
 );
 
 // Carsh Register: { denom1: count1, denom2: count2, ... }
 let CR = {};
 
 /**
- * Sum the cash object up, incl. to the maxDenom denomation
+ * Sum the cash object up, incl., to the maxDenom denomation
  * 
- * @param cash { denom1: count1, denom2: count2, ... }
- * @param maxDenom ignore denominations above this
- * 
+ * @param object cash { denom1: count1, denom2: count2, ... }
+ * @param float maxDenom ignore denominations above this
  * @return float
  */
 const sumCash = (cash, maxDenom = Number.MAX_SAFE_INTEGER) => Object.entries(cash).reduce(
@@ -24,21 +20,22 @@ const sumCash = (cash, maxDenom = Number.MAX_SAFE_INTEGER) => Object.entries(cas
 );
 
 /**
- * @param cr the cash register
- * @param cash the money given
- * @return object the updated register
+ * @param object cr - the cash register
+ * @param object cash - the money given
+ * @return object - the updated register
  */
 const addCash = (cr, cash) => Object.entries(cash).reduce(
     (acc, [d, c]) => {
-        acc[d] = (acc[d] || 0) + c;
+        acc[d] = (acc[+d] || 0) + c;
         if (acc[d] < 0) throw new Error(`Negative value in Cash register, denom.: ${d}, count added: ${c}`);
         return acc;
     }, {...cr}
 );
 
 /**
- * @param cr the cash register
- * @param cash the money given
+ * @param object cr - the cash register
+ * @param object cash - the money given
+ * @param float price - the requested price
  * @return object {
  *      cr: register after +cash/-change
  *      change: the change in the CR format
@@ -48,15 +45,36 @@ const addCash = (cr, cash) => Object.entries(cash).reduce(
 const transcation = (cr, cash, price) => {
     const totalCashGiven = sumCash(cash);
     const totalChange = totalCashGiven - price;
-    if (0 > totalChange) {
+    if (0 > totalChange) { // Too few money given
         return {cr, change: cash, status: TR.INSUFFICIENT};
     }
     const ncr = addCash(cr, cash);
-    if (0 == totalChange) {
+    if (0 == totalChange) { // Exact price given
         return {cr: ncr, change: {}, status: TR.OK };
+    }
+    if (sumCash(ncr, totalChange) < totalChange) { // Not enough money in smaller denoms.
+        return {cr, change: cash, status: TR.NO_CHANGE};
+    }
+    // Try to combine the different denominations in the cr to match the change
+    const changeEntries = _tryComb(
+        Object.entries(ncr).filter(e => e[0] <= totalChange).sort(([a], [b]) => b - a);
+        totalChange
+    );
+    if (! changeEntries) {
+        return {cr, change: cash, status: TR.NO_CHANGE};
     }
     
     
-    
     return {cr, change: cash, status: TR.FAIL};
+}
+
+/**
+ * @param array denoms - Object.entries of the cr with denom up to target, sorted descending
+ * @param float target
+ * @return object|false - combination of denominations | failure
+ */
+const _tryComb = (denoms, target) => {
+    
+    
+    return false;
 }
