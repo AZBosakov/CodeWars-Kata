1e728eaae45a77916115d459b8b805c7386116b9
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2020-09-11 21:59:54 +0300

    Generic enumeration done

--- Permutations.js
+++ Permutations.js
@@ -1,34 +1,43 @@
 
 const permutationGenerator = function(arr) {
+    if (arr.length < 2) return arr;
     // INIT {
-    const sorted = [...arr].sort();
+    const preSort = [...arr].sort();
     /**
+     * Normalization:
      * Associate a consequtive number with each distinct element (0-based).
-     * Duplicate elements map to the same number (the array index).
-     * This way, the algo always works on numbers in range 0..num_dist-1
+     * Duplicate elements map to the same number (the array index of eIdx[]).
+     * This way, the algo always operates on numbers in range 0..num_dist-1
+     * Numbers are assigned in the order of occurence, hence the preSort,
+     * to make numbers increase with the lexicographic order, if any.
      */
-    const eIdx = sorted.reduce((acc, e, i, arr) => {
-        if (e != arr[i - 1]) acc.push(e);
-        return acc;
-    }, []);
-    // Init the first perm. with the indexes of the element
-    const current = sorted.reduce((acc, e, i, arr) => {
-        if (i) {
-            e == arr[i - 1] ? acc.push(acc[i - 1]) : acc.push(acc[i - 1] + 1);
-        } else {
-            acc.push(0);
-        }
-        return acc;
-    }, []);
-    // Bits Per Int
-    const BPI = 32;
+    const els = new Map();
+    preSort.forEach(e => els.set(e, true));
+    // [els.el.0, els.el.1, ...] : maps number => element
+    const eIdx = [...els.entries()].map(e => e[0]);
+    // Reverse mapping: element => number
+    eIdx.forEach((e, i) => els.set(e, i));
+    /**
+     * Init the first lex. permutation.
+     * Now sorted by the corresponding numbers.
+     * Handles cases where .sort() doesn't work, eg.:
+     * o0 = {p:0}; o1 = {p:1}; o2 = {p:2}; inpArr = [o0, o1, o0, o2, o1];
+     * .sort() won't change it to [o0, o0, o1, o1, o2],
+     * but after mapping it to [0, 1, 0, 2, 1], it can be sorted
+     */
+    const current = preSort.map(e => els.get(e)).sort((a, b) => a - b);
+    
+    
+    
+    // BitWidth
+    const BW = 32;
     // Bitfield of directions 0:<, 1:>
-    const dir = new Uint32Array(Math.ceil(eIdx.length / BPI))
+    const dir = new Uint32Array(Math.ceil(eIdx.length / BW))
     // map index to element
     const idx2el = idx => eIdx[idx];
     // } INIT
     
-    console.table(sorted.map((e,i) => ([ e, current[i], eIdx[current[i]] ])));
+    console.table(current.map(e => ([eIdx[e], e])));
     
     
 }
