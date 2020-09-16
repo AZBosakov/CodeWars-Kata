9677d40fd62bdd47dbbaa95fbf72c356df362308
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2020-09-16 23:35:50 +0300

    removed generator, order by lexi

--- Permutations.js
+++ Permutations.js
@@ -1,42 +1,34 @@
 /**
- * @param array arr - The items to be permuted
- * @return Iterator - the permutations, starting with the lowest lexicographicaly
+ * Calculate next lexicographic permutation
+ * 
+ * @param array arr - Elements must have a defined '<' relation
+ * @return array|false - The next permutation or false if the passed one is the last.
  */
-const permutationGenerator = function* (arr) {
-    if (arr.length < 2) return arr;
-    const preSort = [...arr].sort();
-    /**
-     * Normalization:
-     * Map a consequtive number to each distinct element (0-based).
-     * Duplicate elements map to the same number (the array index of eIdx[]).
-     * This way, the algo always operates on numbers in range 0..num_dist-1
-     * Numbers are assigned in the order of occurence, hence the preSort,
-     * to make numbers increase with the lexicographic order, if any.
-     */
-    const els = new Map();
-    preSort.forEach(e => els.set(e, true));
-    // [els.el.0, els.el.1, ...] : maps number => element
-    const eIdx = [...els.entries()].map(e => e[0]);
-    // Reverse mapping: element => number
-    eIdx.forEach((e, i) => els.set(e, i));
-    /**
-     * Init the first lex. permutation.
-     * Now sorted by the corresponding numbers.
-     * Handles cases where .sort() doesn't work, eg.:
-     * o0 = {p:0}; o1 = {p:1}; o2 = {p:2}; inpArr = [o0, o1, o0, o2, o1];
-     * .sort() won't change it to [o0, o0, o1, o1, o2],
-     * but after mapping it to [0, 1, 0, 2, 1], it can be sorted
-     */
-    const currentPerm = preSort.map(e => els.get(e)).sort((a, b) => a - b);
-    // map index to element
-    const idx2el = idx => eIdx[idx];
-    yield currentPerm.map(idx2el);
-    
-    
+const permuteNextLO = arr => {
+    const p = [...arr];
+    let s0 = -1;
+    let s1 = -1;
+    for (let i = p.length; i--;) {
+        if (p[i - 1] < p[i]) {
+            s0 = i - 1;
+            break;
+        }
+    }
+    if (!~s0) return false;
+    for (let i = p.length; i--;) {
+        if (p[s0] < p[i]) {
+            s1 = i;
+            break;
+        }
+    }
+    [p[s0], p[s1]] = [p[s1], p[s0]];
+    return p.slice(0, s0 + 1).concat(p.slice(s0 + 1).reverse());
 }
 
-
+// Kata test function
 const permutations = string => {
-    
-    
+    let p = string.split('').sort();
+    const perms = [p.join('')];
+    while (p = permuteNextLO(p)) perms.push(p.join(''));
+    return perms;
 }
