f636335b0cb820f0df2d233085e7e7c340561dd3
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2020-09-17 23:13:53 +0300

    solver separate from main Kata test function

--- N_queens_puzzle_(with_one_mandatory_queen_position).js
+++ N_queens_puzzle_(with_one_mandatory_queen_position).js
@@ -1,28 +1,25 @@
 /**
- * Calculate the positions of the queens on the boards,
- * given 1 of them.
+ * Solve N-queens for n <= 32
  * 
+ * Return a single solution of the N-queens if any or false otherwise.
  * Artificialy limited to max. size 32 - the bit width of a JS int - 
- * due to the chosen algo. for checking safe squares. According to
- * https://en.wikipedia.org/wiki/Eight_queens_puzzle
- * the max. completely enumerated is 27x27 anyway.
+ * due to the chosen algo. for checking safe squares.
  * 
- * @param string position - The position of the given fixed queen.
  * @param int size : 1..32 - The size of the board - size x size
+ * @param array|false queen - The [row, col] (0-based) of the fixed queen if given
  * @return array|false - The array of queens' positions or false if no solution
  */
-const queens = (position, size) => {
+const queenSolver_32 = (size, queen = false) => {
     // Rows will be represented as bitfields of unsafe squares, so max 32 cols
     const JS_INT_BITS = 32;
     if (JS_INT_BITS < size) throw new Error(`Size too big: ${size}. Max size: ${JS_INT_BITS}`);
     
     if (1 > size || ) throw new Error(`Invalid size: ${size}. Must be positive int`);
     
-    const colNames = 'abcdefghijklmnopqrstuvwxyzABCDEF';
-    const posCol = colNames.indexOf(position[0]);
-    const posRow = position[0] - 1;
+    if (queen[0] > size || queen[1] > size) throw new Error(`Invalid position: ${position}`);
     
-    
-    if (1 == size) return 'a1';
+    if (1 == size) return [0,0];
     if (4 > size) return false;
+    
+    
 }
