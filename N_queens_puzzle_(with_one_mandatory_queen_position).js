6a6b9b12d376a03bfb1e4e9a8ac6cbf850c3b62e
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2020-09-23 01:31:07 +0300

    WIP reqursive placeQueen - written, seems working

--- N_queens_puzzle_(with_one_mandatory_queen_position).js
+++ N_queens_puzzle_(with_one_mandatory_queen_position).js
@@ -1,14 +1,12 @@
 const DEBUG = {
-    attacked: arr => console.log(
-        [...arr].map(e => {
-            let bin = '';
-            for (let i = 0; i < 32; i++) {
-                
-                bin += ( (e & (1 << i)) >>> i );
-            }
-            return bin;
-        }).join("\n")
-    ),
+    bitmap: arr => [...arr].map(e => {
+        let bin = '';
+        for (let i = 0; i < 32; i++) {
+            
+            bin += ( (e & (1 << i)) >>> i );
+        }
+        return bin;
+    }).join("\n"),
 };
 
 /**
@@ -17,6 +15,14 @@ const DEBUG = {
  * Return a single solution of the N-queens if any or false otherwise.
  * Artificialy limited to max. size 32 - the bit width of a JS int - 
  * due to the chosen algo. for checking safe squares.
+ * 
+ * Recursively places queens on safe squares on subsequent rows.
+ * On each placement, marks the lower rows' squares, attacked by this queen,
+ * and pass the marked sqares for the next queen. If the lower queens placement fails,
+ * try the next safe square on the row and repeat the above.
+ * 
+ * NOTE: Haven't compared performance with other algorythms,
+ * but on my ancient Celleron E3300 / Chrome 80 solves 30 queens in ~15 sec
  * 
  * @param int size : 1..32 - The size of the board - size x size
  * @param array|false fixQueen - The [row, col] (0-based) of the fixed queen if given
@@ -36,13 +42,13 @@ const nQueenSolver_max32 = (size, fixQueen = false) => {
     
     // Mark the highest bits above 'size' as attacked
     const maxSafeCols = JS_INT_BITS == size ? 0 : ((1 << 31) >> (31 - size));
-    const attacked = (new Uint32Array(size)).fill(maxSafeCols);
+    const attackedInit = (new Uint32Array(size)).fill(maxSafeCols);
     
     const fixQueenRow = fixQueen ? fixQueen[0] : -1;
     const fixQueenCol = fixQueen ? (1 << fixQueen[1]) : 0;
     
     // Mark the squares, attacked by the fixed queen, if given
-    if (fixQueen) attacked.forEach((e, i, arr) => {
+    if (fixQueen) attackedInit.forEach((e, i, arr) => {
         let diff = Math.abs(i - fixQueenRow);
         if (diff) {
             arr[i] |= (fixQueenCol | (fixQueenCol << diff) | (fixQueenCol >>> diff));
@@ -51,15 +57,30 @@ const nQueenSolver_max32 = (size, fixQueen = false) => {
     
     const placeQueen = (queenRow, attackedSq) => {
         let row = attackedSq[0];
-        const lowerRows = attackedSq.slice(1);
-        // No safe square - row is all 1s == -1
-        if (!~row) return false;
+        // If the fixed queen
         if (fixQueenRow == queenRow) {
             // If on last row
-            if (! lowerRows.length) return [fixQueenCol];
-            const lowerQueens = placeQueen(queenRow + 1, lowerRows);
+            if (1 == attackedSq.length) return [fixQueenCol];
+            const lowerQueens = placeQueen(queenRow + 1, attackedSq.slice(1));
             return lowerQueens ? [fixQueenCol, ...lowerQueens] : false;
         }
-        
+        let safe = 0;
+        while (safe = (~row & (row + 1))) {
+            if (1 == attackedSq.length) return [safe];
+            // Mark the squares on the lower rows attacked by this queen
+            const lowerRows = attackedSq.slice(1)
+            for (let i = lowerRows.length; i--; ) {
+                let d = i + 1;
+                lowerRows[i] |= ((safe << d) | safe | (safe >>> d));
+            }
+            const lowerQueens = placeQueen(queenRow + 1, lowerRows);
+            if (lowerQueens) return [safe, ...lowerQueens];
+            row |= safe;
+        }
+        return false;
     }
+    
+    const result = placeQueen(0, attackedInit);
+    if (! result) return false;
+    
 }
