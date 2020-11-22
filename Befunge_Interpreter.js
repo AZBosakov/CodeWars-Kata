e3787219173d353c884901c9214bff01512cc618
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2020-11-22 21:06:15 +0200

    Random dirs test working, sieve - inf cycle

--- Befunge_Interpreter.js
+++ Befunge_Interpreter.js
@@ -108,27 +108,27 @@ const interpret = code => {
     }
     // } Instruction Set
     
-    let infCycle = 500; // Inf. cycle protection
+    let infCycle = 1000; // Inf. cycle protection
     
     while(!terminate) {
         terminate = --infCycle < 0; // Inf. cycle protection
         const [r, c] = IP;
         const char = grid[r][c];
+        console.group();
+        console.log('IP:',IP);
+        console.log('S bef:', stack, char);
         if (char == STR_MODE) {
             stringMode = !stringMode;
         } else {
-            console.group();
-            console.log('IP:',IP);
-            console.log('S bef:', stack, char);
             
             if (stringMode) {
                 push(String(char).charCodeAt(0));
             } else {
                 IS[char]();
             }
-            console.log('S aft:', stack);
-            console.groupEnd()
         }
+        console.log('S aft:', stack);
+        console.groupEnd()
         moveIP();
     }
     
