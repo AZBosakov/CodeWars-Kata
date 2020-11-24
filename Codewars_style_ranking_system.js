cc137c2b370eb35d29febb564f3370715b682027
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2020-11-24 23:39:04 +0200

    DONE

--- Codewars_style_ranking_system.js
+++ Codewars_style_ranking_system.js
@@ -1,7 +1,8 @@
 const User = (() => {
     const RANKS = [-8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8];
-    const MAX_LEVEL = RANKS.length - 1;
     const SCORE_PER_LEVEL = 100;
+    const MAX_LEVEL = RANKS.length - 1;
+    const MAX_SCORE = MAX_LEVEL * SCORE_PER_LEVEL;
     const RANK_LEVEL = new Map(RANKS.map((e, i) => [e, i]));
     
     const newScore = (curScore, chaRank) => {
@@ -9,33 +10,39 @@ const User = (() => {
         if (undefined === chaLevel) {
             throw new Error(`Invalid rank: ${chaRank}`);
         }
-        const curlevel = (curScore / SCORE_PER_LEVEL)|0;
-        const diff = Math.max(chaLevel - curlevel, -2);
+        const curLevel = (curScore / SCORE_PER_LEVEL)|0;
+        const diff = Math.max(chaLevel - curLevel, -2);
+        
+        let adjust;
         switch (diff) {
-            case -2: return curScore;
-            case -1: return curScore + 1;
-            case 0: return curScore + 3;
-            default: return curScore + 10 * diff**2;
+            case -2:
+                adjust = 0
+                break;
+            case -1:
+                adjust = 1;
+                break;
+            case 0:
+                adjust = 3;
+                break;
+            default:
+                adjust = 10 * diff**2;
         }
+        return Math.min(MAX_SCORE, curScore + adjust);
     }
     
     return function () {
         var score = 0;
-        var level = 0;
         
-        this.incProgress = chaRank => {
-            score = newScore(score, chaRank);
-            level = Math.min(score / SCORE_PER_LEVEL, MAX_LEVEL)|0;
-        }
+        this.incProgress = chaRank => score = newScore(score, chaRank);
         
         Object.defineProperties(
             this,
             {
                 rank: {
-                    get: () => RANKS[level],
+                    get: () => RANKS[(score / SCORE_PER_LEVEL)|0],
                 },
                 progress: {
-                    get: () => score - level * SCORE_PER_LEVEL,
+                    get: () => score % SCORE_PER_LEVEL,
                 },
             }
         );
