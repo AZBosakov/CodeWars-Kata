f23b2d72d374049b7f9dee2994982244a75c455a
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2020-11-24 20:53:05 +0200

    revert to old int generating

--- Befunge_Interpreter.js
+++ Befunge_Interpreter.js
@@ -120,12 +120,12 @@ const interpret = code => {
     let infCycle = 1000; // Inf. cycle protection
     
     while(!terminate) {
-        terminate = --infCycle < 0; // Inf. cycle protection
+//         terminate = --infCycle < 0; // Inf. cycle protection
         const [r, c] = IP;
         const char = grid[r][c];
-        console.group();
-        console.log('IP:',IP);
-        console.log('S bef:', stack, char);
+//         console.group();
+//         console.log('IP:',IP);
+//         console.log('S bef:', stack, char);
         if (char == STR_MODE) {
             stringMode = !stringMode;
         } else {
@@ -135,8 +135,8 @@ const interpret = code => {
                 IS[char]();
             }
         }
-        console.log('S aft:', stack);
-        console.groupEnd()
+//         console.log('S aft:', stack);
+//         console.groupEnd()
         moveIP();
     }
     
