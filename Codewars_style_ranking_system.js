72e4459b2b3dd6404b509867810a15590bc74f7f
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2020-11-24 23:21:40 +0200

    some syntax err, seems working

--- Codewars_style_ranking_system.js
+++ Codewars_style_ranking_system.js
@@ -1,15 +1,16 @@
 const User = (() => {
     const RANKS = [-8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8];
-    const LEVEL_UP = 100;
+    const MAX_LEVEL = RANKS.length - 1;
+    const SCORE_PER_LEVEL = 100;
     const RANK_LEVEL = new Map(RANKS.map((e, i) => [e, i]));
     
-    const newScore = (curScore, chRank) => {
-        const chLevel = RANK_LEVEL.get(chRank|0);
-        if (undefined === chLevel) {
-            throw new Error(`Invalid rank: ${chRank}`);
+    const newScore = (curScore, chaRank) => {
+        const chaLevel = RANK_LEVEL.get(chaRank|0);
+        if (undefined === chaLevel) {
+            throw new Error(`Invalid rank: ${chaRank}`);
         }
-        const curlevel = (curScore / LEVEL_UP)|0;
-        const diff = Math.max(chLevel - curlevel, -2);
+        const curlevel = (curScore / SCORE_PER_LEVEL)|0;
+        const diff = Math.max(chaLevel - curlevel, -2);
         switch (diff) {
             case -2: return curScore;
             case -1: return curScore + 1;
@@ -18,9 +19,27 @@ const User = (() => {
         }
     }
     
-    return function {
+    return function () {
         var score = 0;
+        var level = 0;
         
+        this.incProgress = chaRank => {
+            score = newScore(score, chaRank);
+            level = Math.min(score / SCORE_PER_LEVEL, MAX_LEVEL)|0;
+        }
         
+        Object.defineProperties(
+            this,
+            {
+                rank: {
+                    get: () => RANKS[level],
+                },
+                progress: {
+                    get: () => score - level * SCORE_PER_LEVEL,
+                },
+            }
+        );
+        
+        Object.freeze(this);
     }
 })();
