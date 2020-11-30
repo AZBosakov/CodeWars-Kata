8478dd1be0e473f728ff8b05d31d4cb1f003f88c
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2020-11-30 18:34:38 +0200

    WIP -rework

--- Sudoku_Solver.js
+++ Sudoku_Solver.js
@@ -1,38 +1,58 @@
 /**
- * Generate a solver for sudokus 1x1 / 4x4 / 9x9 / 25x25
- * Limit to 25 due to using 32bit bitfields, and 25 is the largest square bellow 32.
+ * Generate a solver for square sudokus
  * 
- * @param int minorSquare : 1-5: The row length/count in a row of the minor squares
- * @param any emptyCell : The designator for unfilled cells
- * @param array numerals : The symbols used for the numbers. Must be of length at least minorSquare**2 
- * @return function : The solver for sudoku of size minorSquare**2 x minorSquare**2
+ * @param array[array[string|number]] template :
+ *      A square array of symbols, distinct symbols belonging to different regions.
+ *      Each symbol must occur n-times, where n is the length of the square of the puzzle
+ * @param string|number emptyCell : The symbol designating unfilled cells
+ * @param array[string|number] numerals : The symbols used for the numbers.
+ * @return function : The solver for the sudoku type
  */
-const createSudokuSolver_max5 = (
-    minorSquare = 3, // minor squares: 3x3
+const createSudokuSolver = (
+    template = [
+        [0,0,0,1,1,1,2,2,2],
+        [0,0,0,1,1,1,2,2,2],
+        [0,0,0,1,1,1,2,2,2],
+        [3,3,3,4,4,4,5,5,5],
+        [3,3,3,4,4,4,5,5,5],
+        [3,3,3,4,4,4,5,5,5],
+        [6,6,6,7,7,7,8,8,8],
+        [6,6,6,7,7,7,8,8,8],
+        [6,6,6,7,7,7,8,8,8],
+    ],
     emptyCell = 0,
     numerals = [
-        1,  2,  3,  4,  5,
-        6,  7,  8,  9,  10,
-        11, 12, 13, 14, 15,
-        16, 17, 18, 19, 20,
-        21, 22, 23, 24, 25
+        1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16,
+        17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
     ] // Default, usefull for the usual 1-9 or the max size
 ) => {
-    if (minorSquare < 1 || minorSquare > 5) {
-        throw new Error(`Minor square size out of range (1..5): ${minorSquare}`);
+    const SIZE = template.length;
+    const INT_WIDTH = 32;
+    if (SIZE < 1 || SIZE > INT_WIDTH) {
+        throw new Error(`Invalid template size (1-32): ${SIZE}`);
     }
-    const ROW_LEN = minorSquare ** 2;
-    if (numerals.length < ROW_LEN) {
+    if (numerals.length < SIZE) {
         throw new Error(
-            `Not enough numerals (${numerals.length} provided) for sudoku of ${ROW_LEN}x${ROW_LEN}`
+            `Not enough numerals (${numerals.length} provided) for sudoku of ${SIZE}x${SIZE}`
         );
     }
-    const NUMERAL_INDEX = new Map(numerals.map((e, i) => [String(e), i]));
-    if (NUMERAL_INDEX.size < numerals.length || NUMERAL_INDEX.has(String(emptyCell))) {
+    const NUM_IDX = new Map(numerals.map((e, i) => [String(e), i]));
+    if (NUM_IDX.size < numerals.length || NUM_IDX.has(String(emptyCell))) {
         throw new Error(`Duplicate numerals or empty cell symbol used as numeral!`);
     }
-    NUMERAL_INDEX.set(String(emptyCell), -1);
+    NUM_IDX.set(String(emptyCell), -1);
+    const regSyms = new Map();
+    template.forEach(row => row.forEach(
+        cell => {
+            cell = String(cell);
+            const enc = regSyms.get(cell)|0;
+            regSyms.set(cell, enc + 1);
+        }
+    ));
+    const ents = [...regSyms.entries()];
     
+    
+    const INIT_BITS = SIZE == 32 ? 0 : (-1 << SIZE);
     /**
      * Solve the sudoku
      * 
@@ -40,55 +60,26 @@ const createSudokuSolver_max5 = (
      * @return array[array] : 2 dim. array[r][c] of the filled sudoku cells
      */
     return sudoku => {
-        if (sudoku.length != ROW_LEN) {
-            throw new Error(`Invalid row count`);
+        if (sudoku.length != SIZE) {
+            throw new Error(`Invalid size: ${sudoku.length}. Must be ${SIZE}x${SIZE}`);
         }
-        const INIT_BITS = ~((1 << ROW_LEN) - 1);
-        const MIN_SQ = minorSquare;
-        // Bitmask Index
-        const [
-            BI_ROWS,
-            BI_COLS,
-            BI_SQUS,
-        ] = [0, 1, 2];
-        // Bitmasks for numbers already present in the row/col/minor square
-        const INIT_BITMASKS = [
-            Array(ROW_LEN).fill(INIT_BITS),
-            Array(ROW_LEN).fill(INIT_BITS),
-            Array(ROW_LEN).fill(INIT_BITS),
-        ];
+        // Bitmasks for used numbers. 0s are for numbers, not yet used in a row/col/region
+        const ROW = Array(SIZE).fill(INIT_BITS);
+        const COL = Array(SIZE).fill(INIT_BITS);
+        const REG = Array(SIZE).fill(INIT_BITS);
         
-        // The minor square of the cell [r,c]
-        const msq = (r, c) => ((r / MIN_SQ)|0) * MIN_SQ + ((c / MIN_SQ)|0);
-        
-        // Mark a number as used in the corresponding row, col. and minor square
-        const setBit = (bms, r, c, v, cloneBM = true) => {
-            const nbms = cloneBM ? bms.map(bm => bm.slice()) : bms;
-            nbms[BI_ROWS][r] |= v;
-            nbms[BI_COLS][c] |= v;
-            nbms[BI_SQUS][msq(r, c)] |= v;
-            return nbms;
-        }
-        /**
-         * Get a bitmask of the possible numbers for the [row, col]
-         * An intersection of the sets of unused numbers for the
-         * corresponding row, col. and minor square
-         */
-        const getBits = (bms, r, c) => ~(
-            bms[BI_ROWS][r] | bms[BI_COLS][c] | bms[BI_SQUS][msq(r, c)]
-        );
         
         const normalized = sudoku.map(
             (row, ri) => {
-                if (row.length != ROW_LEN) {
-                    throw new Error(`Invalid cell count in row ${ri}`);
+                if (row.length != SIZE) {
+                    throw new Error(`Not a square: invalid cell count in row ${ri}`);
                 }
                 return row.map(
                     (cell, ci) => {
-                        if (! NUMERAL_INDEX.has(String(cell))) {
+                        if (! NUM_IDX.has(String(cell))) {
                             throw new Error(`Undefined numeral at [${ri}][${ci}]`);
                         }
-                        return NUMERAL_INDEX.get(String(cell));
+                        return NUM_IDX.get(String(cell));
                     }
                 );
             }
@@ -149,4 +140,4 @@ const createSudokuSolver_max5 = (
     }
 }
 
-const sudoku = createSudokuSolver_max5(3, 0, [1,  2,  3,  4,  5, 6,  7,  8,  9]);
+const sudoku = createSudokuSolver();
