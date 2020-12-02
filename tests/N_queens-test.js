97afd0dfb64beaf5e49308e656a5900c9fa309c6
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2020-12-02 17:11:31 +0200

    test funcs - create tpl

---
+++ tests/N_queens-test.js
@@ -0,0 +1,25 @@
+console.log('Load N-queen test');
+
+const nqTest = (size, fixQueen = false) => {
+    const qs = nQueenSolver_max32(size, fixQueen)
+    const target = document.getElementById('target>');
+    target.innerHTML = '';
+    const table = document.createElement('table');
+    for (let r = 0; r < qs.length; r++) {
+        let row = document.createElement('tr');
+        for (let c = 0; c < qs.length; c++) {
+            let cell = document.createElement('td');
+            Object.assign(cell.style, {
+                width: '2rem',
+                height: '2rem',
+                backgroundColor: ((r + c) & 1) ? '#ddd' : '#777',
+                textAlign: 'center',
+                fontSize: '1.5rem',
+            });
+            if (qs[r] == c) cell.textContent = '♛';
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+    target.appendChild(table);
+}
