09c0cf573bf9197f16088637c8c6f6ef0cf903ce
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2021-01-28 19:33:17 +0200

    extracted pos. multiplier ina func

--- tests/Help_the_general_decode.js
+++ tests/Help_the_general_decode.js
@@ -1,4 +1,4 @@
-const TEST_STR = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+const TEST_STR = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
 
 // const TEST_STR = 'What is this ?';
 
@@ -13,21 +13,35 @@ const INDEX = ALPHABET.split('').reduce(
 );
 INDEX.delete('_');
 
+// position % ALPHA_LEN => Multiplier Modulus for the symbol index
+// pos => 2**(pos % ALPHA_LEN + 1)
+const PC = 50; // power chunks: 2**50 < Number.MAX_SAFE_INTEGER;
+const mm = pos => {
+    const pwr = pos % ALPHA_LEN + 1;
+    let x = 1;
+    for (let i = (pwr/PC)|0; i; i--) {
+        x *= 2**PC % CYCLE;
+    }
+    return x * 2**(pwr % PC) % CYCLE;
+}
 
 const enc = (char, pos) => {
     const i = INDEX.get(char);
-    const ei = (i * 2**(pos % ALPHA_LEN + 1)) % CYCLE;
+    if (! i) return char;
+    const ei = i * mm(pos) % CYCLE;
     return ALPHABET[ei];
 }
 
 const dec = (char, pos) => {
     const ei = INDEX.get(char);
+    if (! i) return char;
     pos = pos % ALPHA_LEN;
+    
     const mul = 2**(pos % ALPHA_LEN + 1);
     
 //     const di = 
     
     return ALPHABET[di];
 }
-
-console.log(TEST_STR.split('').map(enc).join(''));
+e = s => s.split('').map(enc).join('');
+// console.log(TEST_STR.split('').map(enc).join(''));
