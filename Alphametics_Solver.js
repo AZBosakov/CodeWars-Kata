3b269ac2651bcc98f726cccf1556abd82b199817
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2021-04-02 01:03:16 +0300

    WIP - some utils

--- Alphametics_Solver.js
+++ Alphametics_Solver.js
@@ -3,18 +3,16 @@
 'use strict';
 
 // const alphametics = (() => { //TODO
-    
-    
+    // code convention: all the $-prefixed vars/funcs are bitfields
     const BASE = 10;
     // Bits 0..BASE-1, corresponding to each digit
     const [$0, $1, $2, $3, $4, $5, $6, $7, $8, $9] = [...Array(BASE).keys()].map(e => 1 << e);
-    // Util: 
-    const $ = (...digList) => digList.reduce((acc, e) => acc | (1 << e) , 0);
     
-    const ALL = (1 << BASE) - 1;
-    const ODD = ALL & 0xAAAAAAAA; // bits 1,3,5,...
-    const EVEN = ALL & ~ODD; // bits 0,2,4...
-
+    const $ALL = (1 << BASE) - 1;
+    // Utils: digits => bitfield
+    const $ = (...digList) => digList.reduce((acc, e) => (acc | (1 << e)) & $ALL , 0);
+    const $lt = digit => $ALL & ((1 << digit) - 1);
+    const $gt = digit => $ALL & ~((1 << ++digit) - 1);
     
 //     return s => { //TODO
     const alphametics = puzzle => {
