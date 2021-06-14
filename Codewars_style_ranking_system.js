dca08231eebe4068fe0aaff90ef620c5b808fe59
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2021-06-14 20:56:21 +0300

    added missing codewars link

--- Codewars_style_ranking_system.js
+++ Codewars_style_ranking_system.js
@@ -1,3 +1,6 @@
+// https://www.codewars.com/kata/51fda2d95d6efda45e00004e
+// 4 kyu
+
 const User = (() => {
     const RANKS = [-8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8];
     const SCORE_PER_LEVEL = 100;
