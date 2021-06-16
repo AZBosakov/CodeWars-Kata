fbce7f16c05ea30938970e919d8082cf3cd175fa
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2021-06-16 19:46:24 +0300

    New kata

---
+++ Arbitrary-Length_Integer_Arithmetic.js
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/530e69ae72d6dfced0000a9e
+
+const {
+    add, subtract, multiply, divide
+} = (() => {
+    // Use base 10^12 for add/sub, instead of individual digits
+    const ADD_BASE = 12;
+    
+    
+    
+    
+    return {
+        add: (a, b) => {
+            
+            return result;
+        },
+        subtract: (a, b) => {
+            
+            return result;
+        },
+        multiply: (a, b) => {
+            
+            return result;
+        },
+        divide: (a, b) => {
+            
+            return result;
+        },
+    }
+})();
