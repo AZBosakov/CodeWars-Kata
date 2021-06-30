fcf7290f9bda4db11785afd8ba53cdc82478ea03
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2021-06-30 21:32:40 +0300

    base complement tested for 10**1

--- Arbitrary-Length_Integer_Arithmetic.js
+++ Arbitrary-Length_Integer_Arithmetic.js
@@ -2,33 +2,87 @@
 
 /**
  * The kata requires only positive ints,
- * but coding floats support as an excersize
+ * but I'm coding floats support as an excersize
  */
-
+/*
 const {
     add, subtract, multiply, divide
-} = (() => {
+} = (() => {*/
     /**
-     * Use base 10^15 for add/sub, instead of individual digits
-     * Sum/diff of 2 15-digits < Number.MAX_SAFE_INTEGER
+     * Use base 10^SUM_B_10E for add/sub, instead of individual digits
+     * Choose SUM_B_10E so sum/diff of SUM_B_10E-digits < Number.MAX_SAFE_INTEGER
      */
-    const SUM_BASE = 15;
+    const SUM_B_10E = 1; // 12;
+    const SUM_MOD = 10**SUM_B_10E;
+    const N_NINES = SUM_MOD - 1; // for the complements
     
-    // UTIL: parse float: '-123.456e-3' => {s: -1, i: 123456, e: -6}
+    const MUL_BASE_10E = 8;
+    
+    // UTIL: parse float: '-123.456e-3' => {s: -1, m: 123456, e: -6}
     const f2sie = fStr => {
         const parse = fStr.match(/^\s*([+-]?)(\d+(?:\.\d*)?|\d*\.\d+)(?:e([+-]?\d+))?/i);
-        if (! parse) return {s: NaN, i: NaN, e: NaN};
+        if (! parse) return null;
         const [, int, frac] = parse[2].match(/^(\d*)\.?(\d*)$/);
-        const [,i, zs] = (int + frac).match(/^0*(\d+?)(0*)$/);
+        const [,m, zs] = (int + frac).match(/^0*(\d+?)(0*)$/);
         return {
-            i,
-            s: i == '0' ? 0 : (parse[1] + 1)|0,
-            e: i == '0' ? 0 : (parse[3]|0) - frac.length + zs.length,
+            m,
+            s: m == '0' ? 0 : (parse[1] + 1)|0,
+            e: m == '0' ? 0 : (parse[3]|0) - frac.length + zs.length,
         };
     }
     
+    // UTIL: Negate the number in its object representation
+    const neg = fo => ({...fo, s: fo.s * -1});
     
+    // UTIL: 10**SUM_B_10E complement
+    const b10ECmpl = digList => {
+        let carry = 1;
+        return digList.map(e => {
+            const cmpl = N_NINES - e + carry;
+            carry = Math.floor(cmpl / SUM_MOD);
+            return cmpl % SUM_MOD;
+        });
+    }
     
+    // UTIL: split string into groups of digits, from the LEFT
+    const chunk = (str, n = 1) => {
+        const dl = [];
+        let e = 0;
+        let chunk = '';
+        while (
+            chunk = str.slice(e - n, e || undefined)
+        ) {
+            dl.push(parseFloat('0' + chunk));
+            e -= n;
+        }
+        return dl;
+    }
+    
+    const sum = (...fos) => {
+        // find min common exponent
+        const resultExp = Math.min(...fos.map(fo => fo.e));
+        // right pad with 0s, to equalize the exponents
+        const strs = fos.map(fo => fo.m + '0'.repeat(fo.e - resultExp));
+        // split strings into SUM_B_10E-length chunks, FROM LEFT
+        const digLists = strs.map(str => chunk(str, SUM_B_10E));
+        // max addition columns
+        const maxDigits = digLists.map(
+            e => e.length
+        ).reduce(
+            (acc, e) => Math.max(acc, e), 0
+        ) + Math.round(Math.log10(digLists.length) + 0.5) + 2;
+        // +2 - reserve place for complement carry ^^
+        // right 0-pad to equalize lengths
+        digLists.forEach(dl => {
+            dl.push(...Array(maxDigits - dl.length).fill(0));
+        });
+        
+            console.log('before 10compl', digLists);
+            
+        const digListCmpl = digLists.map((dl, i) => (~fos[i].s) ? dl : b10ECmpl(dl));
+            console.log('after 10compl', digListCmpl);
+    }
+/*    
     return {
         add: (a, b) => {
             
@@ -47,4 +101,4 @@ const {
             return result;
         },
     }
-})();
+})();*/
