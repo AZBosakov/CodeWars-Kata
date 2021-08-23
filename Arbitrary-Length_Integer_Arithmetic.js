debf389e5e65289f40cd50df7416bd1c36eaabd9
Aleksandar Zdravkov Bosakov <aleksandar.z.bosakov@gmail.com>
2021-08-23 16:54:42 +0300

    aggregated the digit list functions into a DL {}

--- Arbitrary-Length_Integer_Arithmetic.js
+++ Arbitrary-Length_Integer_Arithmetic.js
@@ -78,69 +78,24 @@
             return Object.freeze(obj);
         }
     })();
-        
-    // UTIL: left pad with 0
-    const lp0 = n => '0'.repeat(BASE10E - String(n).length) + n;
     
-    // UTIL: split string into digits in BASE10E, from the LEFT
-    const split10E = str => {
-        const dl = [];
-        let e = 0;
-        let chunk = '';
-        while (
-            chunk = str.slice(e - BASE10E, e || undefined)
-        ) {
-            dl.push(parseFloat('0' + chunk));
-            e -= BASE10E;
-        }
-        dl[SED] = 0;
-        return dl;
-    }
-    
-    const join10E = dl => dl.map(lp0).reverse().join('');
-    
-    const signExtend = (dl, i) => (i < dl.length) ? dl[i] : (dl[SED] || 0);
-    
-    /**
-     * Remove the most signifficant sign digits, equal to [SED]
-     * Does not create new array - modifies the passed one
-     */
-    const trim = dl => {
-        dl[SED] = dl[SED] || 0;
-        let i = dl.length;
-        while (--i && dl[i] == dl[SED]) ;
-        dl.length = i+1;
-        return dl;
-    }
-    
-    /**
-     * create two separate adders, to avoid creating additional
-     * BASE complement array for subtraction
-     * 'add' does direct carry addition, 'sub' first does a BASE complement.
-     * 
-     * The sign extension digit is stored in [SED] prop of the array.
-     */
-    const adder = ['add', 'sub'].reduce((obj, op, opIdx) => {
-        obj[op] = (a, b) => {
-            const CMPL = opIdx; // BASE complement needed?
-            let carry = opIdx; // 0 || 1
-            const maxDigits = Math.max(a.length, b.length) + 2; // +2 for carry + SED
-            const result = [];
-            for (let i = 0; i < maxDigits; i++) {
-                const ai = signExtend(a, i);
-                let bi = signExtend(b, i);
-                if (CMPL) bi = N_NINES - bi;
-                const si = ai + bi + carry;
-                carry = Math.floor(si / BASE);
-                result.push(si % BASE);
+    // NS for DigitList functions
+    const DL = {
+        // split string into digits in BASE10E, from the LEFT
+        fromString: digits => {
+            const dl = [];
+            let e = 0;
+            let chunk = '';
+            while (
+                chunk = digits.slice(e - BASE10E, e || undefined)
+            ) {
+                dl.push(parseFloat('0' + chunk));
+                e -= BASE10E;
             }
-            result[SED] = result[result.length - 1];
-            return trim(result);
-        }
-        return obj;
-    }, {});
-    
-    const digitList = {
+            dl[SED] = 0;
+            return dl;
+        },
+        stringify: dl => dl.map(n => '0'.repeat(BASE10E - (n+'').length) + n).reverse().join(''),
         is0: dl => !(dl[SED] || ~dl.findIndex(d => d)),
         /**
          * return the power of BASE for digit lists 1,0,0...
@@ -164,25 +119,64 @@
             return power;
         },
         leftShift: (dl, n) => { //TODO
-            
+            if (n < 0) throw new RangeException(`Invalid left shift: ${n}`);
+            // TODO
         },
-    }
+        signExtend: (dl, i) => (i < dl.length) ? dl[i] : (dl[SED] || 0),
+        /**
+        * Remove the most signifficant sign digits, equal to [SED]
+        * Does not create new array - modifies the passed one
+        */
+        trim: dl => {
+            dl[SED] = dl[SED] || 0;
+            let i = dl.length;
+            while (--i && dl[i] == dl[SED]) ;
+            dl.length = i+1;
+            return dl;
+        },
+    };
+    
+    /**
+     * create two separate adders, to avoid creating additional
+     * BASE complement array for subtraction
+     * '.add' does direct carry addition, '.sub' first does a BASE complement.
+     * 
+     * The sign extension digit is stored in [SED] prop of the array.
+     */
+    ['add', 'sub'].forEach((op, opIdx) => {
+        DL[op] = (a, b) => {
+            const CMPL = opIdx; // BASE complement needed?
+            let carry = opIdx; // 0 || 1
+            const maxDigits = Math.max(a.length, b.length) + 2; // +2 for carry + SED
+            const result = [];
+            for (let i = 0; i < maxDigits; i++) {
+                const ai = DL.signExtend(a, i);
+                let bi = DL.signExtend(b, i);
+                if (CMPL) bi = N_NINES - bi;
+                const si = ai + bi + carry;
+                carry = Math.floor(si / BASE);
+                result.push(si % BASE);
+            }
+            result[SED] = result[result.length - 1];
+            return DL.trim(result);
+        }
+    });
     
     const sum = (...fos) => {
         // find min common exponent
         const resultExp = Math.min(...fos.map(fo => fo.exp));
         // right pad with 0s, to equalize the exponents
         const strs = fos.map(fo => fo.digits + '0'.repeat(fo.exp - resultExp));
-        let resDL = strs.map(str => split10E(str)).reduce(
-            (sum, dl, dli) => (fos[dli].sign > 0) ? adder.add(sum, dl) : adder.sub(sum, dl),
+        let resDL = strs.map(str => DL.fromString(str)).reduce(
+            (sum, dl, dli) => (fos[dli].sign > 0) ? DL.add(sum, dl) : DL.sub(sum, dl),
             [0]
         );
         let sign = '';
         if (resDL[SED]) {
-            resDL = adder.sub([0], resDL);
+            resDL = DL.sub([0], resDL);
             sign = '-';
         }
-        return PF(sign + join10E(resDL) + 'e' + resultExp);
+        return PF(sign + DL.stringify(resDL) + 'e' + resultExp);
     }
     
     const mul = (a, b) => {
